bfb9f9685140b916ba571974918a0f25cf5af628
Morevq <zhenulik5@gmail.com>
2021-02-19 14:25:32 +0300

    Создание шкафа и двери

--- RoomPlanner/RoomPlanner/MainWindow.xaml.cs
+++ RoomPlanner/RoomPlanner/MainWindow.xaml.cs
@@ -68,5 +68,58 @@ namespace RoomPlanner
             ObjWidth.Background = Brushes.White;
             PropertyList.Visibility = Visibility.Hidden;
         }
+        private void CreateRoomButton_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            room = new Rectangle()
+            {
+                Width = 100,
+                Height = 50,
+                Stroke = Brushes.Black,
+                Fill = Brushes.Black
+            };
+            Canvas.SetLeft(room, 800);
+            Canvas.SetRight(room, 900);
+            Canvas.SetTop(room, 500);
+            WorkTable.Children.Add(room);
+        }
+
+        private void CreateRoomButton_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+            PathGeometry pathGeom = new PathGeometry();
+            Path p = new Path();
+
+            LineSegment vertLS = new LineSegment();
+            PathFigure vertPF = new PathFigure();
+            vertPF.StartPoint = new Point(650, 600);
+            vertLS.Point = new Point(600, 600);
+            vertPF.Segments.Add(vertLS);
+            pathGeom.Figures.Add(vertPF);
+
+            LineSegment horLS = new LineSegment();
+            PathFigure horPF = new PathFigure();
+            horPF.StartPoint = new Point(650, 600);
+            horLS.Point = new Point(650, 550);
+            horPF.Segments.Add(horLS);
+            pathGeom.Figures.Add(horPF);
+
+            ArcSegment arc = new ArcSegment();
+            PathFigure arcfrst = new PathFigure();
+            //arcfrst.IsClosed = true;
+            arc.Point = new Point(600, 600);
+            arcfrst.StartPoint = new Point(650, 550);
+            arc.Size = new Size(75, 75);
+            //arcfrst.Size = new Size(250, 250);
+            //arcfrst.IsClosed = false;
+            arcfrst.Segments.Add(arc);
+            pathGeom.Figures.Add(arcfrst);
+
+
+            p.Data = pathGeom;
+            p.Stroke = Brushes.Green;
+            Canvas.SetLeft(p, 800);
+            Canvas.SetRight(p, 900);
+            Canvas.SetTop(p, 500);
+            Grid.Children.Add(p);
+        }
     }
 }
