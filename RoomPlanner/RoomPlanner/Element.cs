60a03a2c7d7f3caf19f32d999b4bccdb189bd3db
Morevq <zhenulik5@gmail.com>
2021-03-23 22:11:46 +0300

    Перемещение шкафа

--- RoomPlanner/RoomPlanner/Element.cs
+++ RoomPlanner/RoomPlanner/Element.cs
@@ -13,12 +13,15 @@ namespace RoomPlanner
 {
     abstract public class Element
     {
+        protected int width;
+        protected int height;
         public abstract int Width { get; set; }
         public abstract int Height { get; set; }
 
         public MainWindow mainWindow;
 
         public abstract void ClickOnElement(object sender, MouseButtonEventArgs e);
+        //public abstract void Btn_OnMouseDown(object sender, MouseButtonEventArgs e);
     }
 
     public class Room : Element
@@ -38,9 +41,11 @@ namespace RoomPlanner
             Canvas.SetLeft(element, (mainWindow.WorkTable.ActualWidth - width) / 2);
             Canvas.SetTop(element, (mainWindow.WorkTable.ActualHeight - height) / 2);
             element.AddHandler(Rectangle.MouseLeftButtonUpEvent, new MouseButtonEventHandler(ClickOnElement));
+            //element.AddHandler(Rectangle.MouseLeftButtonUpEvent, new MouseButtonEventHandler(Btn_OnMouseDown));
             mainWindow.WorkTable.Children.Add(element);
+            
         }
-
+        private object currobj = null;
         public override void ClickOnElement(object sender, MouseButtonEventArgs e)
         {
             Rectangle selectedElement = (Rectangle)sender;
@@ -48,16 +53,21 @@ namespace RoomPlanner
             mainWindow.ObjHeight.Text = Convert.ToString(selectedElement.Height);
             mainWindow.ObjWidth.Text = Convert.ToString(selectedElement.Width);
         }
+        
+        private void MouseEvent(object sender, MouseEventArgs e)
+        {
+            currobj = sender;
+        }
 
         public override int Width
         {
             get
             {
-                return Width;
+                return width;
             }
             set
             {
-                Width = value;
+                width = value;
             }
         }
 
@@ -65,11 +75,11 @@ namespace RoomPlanner
         {
             get
             {
-                return Height;
+                return height;
             }
             set
             {
-                Height = value;
+                height = value;
             }
         }
     }
--- RoomPlanner/RoomPlanner/MainWindow.xaml.cs
+++ RoomPlanner/RoomPlanner/MainWindow.xaml.cs
@@ -31,28 +31,44 @@ namespace RoomPlanner
                     Room room1 = new Room(this);
                     break;
                 case "CreateCupboardButton":
-                    //Rectangle room = new Rectangle()
-                    //{
-                    //    Width = 100,
-                    //    Height = 50,
-                    //    Stroke = Brushes.Black,
-                    //    Fill = Brushes.Black
-                    //};
-                    //Canvas.SetLeft(room, 800);
-                    //Canvas.SetRight(room, 900);
-                    //Canvas.SetTop(room, 500);
-                    //WorkTable.Children.Add(room);
+                    Rectangle room = new Rectangle()
+                    {
+                        Width = 100,
+                        Height = 50,
+                        Stroke = Brushes.Black,
+                        Fill = Brushes.Black
+                    };
+                    Canvas.SetLeft(room, 800);
+                    Canvas.SetRight(room, 900);
+                    Canvas.SetTop(room, 500);
+                    WorkTable.Children.Add(room);
+                    room.MouseLeftButtonDown += ClickOnObject;
+                    //room.MouseMove += MoveObject;
+                    room.MouseLeftButtonUp += DeclineObject;
                     //room.AddHandler(Rectangle.MouseLeftButtonUpEvent, new MouseButtonEventHandler(Rectangle_MouseLeftButtonUp));
                     break;
             }
+        }
+        private Rectangle currobj = null;
+        public void ClickOnObject(object sender, MouseButtonEventArgs e)
+        {
+            //MessageBox.Show("Yes");
+            Rectangle selectedElement = (Rectangle)sender;
+            this.PropertyList.Visibility = Visibility.Visible;
+            currobj = (Rectangle)sender;
+        }
+        
+        public void DeclineObject(object sender, MouseButtonEventArgs e)
+        {
+            currobj = null;
         }
 
         private void CompleteButton_Click(object sender, RoutedEventArgs e)
         {
             //room.Height = Convert.ToDouble(ObjHeight.Text);
             //room.Width = Convert.ToDouble(ObjWidth.Text);
-            //ObjHeight.Background = Brushes.White;
-            //ObjWidth.Background = Brushes.White;
+            ObjHeight.Background = Brushes.White;
+            ObjWidth.Background = Brushes.White;
             //PropertyList.Visibility = Visibility.Hidden;
         }
 
@@ -82,18 +98,40 @@ namespace RoomPlanner
             arcfrst.StartPoint = new Point(650, 550);
             arc.Size = new Size(75, 75);
             //arcfrst.Size = new Size(250, 250);
-            //arcfrst.IsClosed = false;
+            arcfrst.IsClosed = false;
             arcfrst.Segments.Add(arc);
             pathGeom.Figures.Add(arcfrst);
 
 
             p.Data = pathGeom;
-            p.Width = 100;
+            //p.Width = 100;
             p.Stroke = Brushes.Green;
             Canvas.SetLeft(p, 800);
             Canvas.SetRight(p, 900);
             Canvas.SetTop(p, 500);
             Grid.Children.Add(p);
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point point = e.GetPosition(WorkTable);
+            if (currobj != null)
+            {
+                Canvas.SetTop(currobj, point.Y);
+                Canvas.SetLeft(currobj, point.X);
+                //= new Thickness(point.X, point.Y, 0, 0);
+                Console.WriteLine(currobj.Margin.Top.ToString() + " " + currobj.Margin.Left.ToString() + " " + point.X.ToString() + " " + point.Y.ToString() + " ");
+            }
+            //MessageBox.Show(currobj.Margin.Top);
+            //Console.WriteLine(currobj.Margin.Top);
+            //currobj.Margin.Left
+            //Top
+            //currobj.GetType().GetProperty("Location").SetValue(currobj, new Point(point.X, point.Y - 50));
+
+        }
+        private void WorkTable_MouseMove(object sender, MouseEventArgs e)
+        {
+
         }
     }
 }
